8789f6cc850c984abc6e633146d1888c54db1d13
Roni Sore <ronisore360@gmail.com>
2026-01-06 16:33:26 +0800

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,21 @@ const app = express();
 // Allow requests from the frontend and allow cookies to be sent
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000", // for local development
-      "http://localhost:5173", // for Vite local dev
-      "https://blog-app-git-master-ronis-projects-0f22d049.vercel.app", // your Vercel URL
-    ],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (
+        origin === "http://localhost:5173" ||
+        origin === "http://localhost:3000" ||
+        origin ===
+          "https://blog-app-git-master-ronis-projects-0f22d049.vercel.app" ||
+        origin.includes("vercel.app")
+      ) {
+        return callback(null, true);
+      }
+
+      callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   })
 );
